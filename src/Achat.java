56605aef0e4adf993eaf2effc6d1ea2be645d786
StephKevin <kevinstephano55@gmail.com>
2024-03-27 15:14:23 +0100

    ajout de la methodes acheter

--- src/Achat.java
+++ src/Achat.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class Achat {
     protected String[] articles = {"pomme", "fraise", "banane","mangue"};
     protected double[] prix ={19.0, 50.0, 20.0, 13.0};
+    protected String[] acquisitions = new java.lang.String[0];
     protected static int instanceA;
     protected double totale;
     protected int taille;
     public Achat(int pTaille){//Création d'un tableau d'articles vide
         instanceA++;
         this.taille = pTaille;
-        this.articles = new String[pTaille];
+        this.acquisitions = new String[pTaille];
     }
     public static int getInstanceA(){return instanceA;}
     public String[] getArticles(){return articles;}
@@ -23,5 +26,31 @@ public class Achat {
 
     public void setArticles(String[] articles) {
         this.articles = articles;
+    }
+    public void acheter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Remplissez votre pannier ^-^ ");
+        for (int i = 0; i < this.taille; i++){
+            System.out.println("Articles "+i+" :");
+            acquisitions[i] =sc.nextLine();
+        }
+    }
+    public void rechercher() {
+        double price = 0.0;
+        int i = 0;
+        int j = 0;
+        while (i < this.taille) {
+            while (j < 4) {
+                if (this.acquisitions[i] == this.articles[j]) {
+                    price = price + this.prix[j];
+                }
+                else {
+                    j++;
+                }
+            }
+            j = 0;
+            i++;
+        }
+        System.out.println(price);
     }
 }
